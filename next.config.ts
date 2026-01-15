096f1b327593fc46e2237891cf5b179d2c87e1ad
munadi1406 <munadifathullah123@gmail.com>
2026-01-15 11:31:14 +0800

    oke

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['sequelize', 'mysql2', 'yahoo-finance2'],
-  experimental: {
-    turbo: {},
-  },
 };
 
 export default nextConfig;
