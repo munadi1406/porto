e49159c14ad8187bc0946a5dae74a80aa38b9517
munadi1406 <munadifathullah123@gmail.com>
2026-08-28 15:07:09 +0800

    fix: layout provider nesting WsProvider/QueryProvider/LocaleProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,34 +28,34 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <ThemeInit />
         <LocaleProvider>
-        <QueryProvider>
-          <WsProvider>
-            <TooltipProvider>
-            <TopProgressBar />
-            <Toaster position="top-right" />
-            <SidebarProvider
-              style={
-                {
-                  "--sidebar-width": "calc(var(--spacing) * 72)",
-                  "--header-height": "calc(var(--spacing) * 12)",
-                } as React.CSSProperties
-              }
-            >
-              <AppSidebar variant="inset" />
-              <SidebarInset>
-                <SiteHeader />
-                <div className="flex flex-1 flex-col">
-                  <div className="@container/main flex flex-1 flex-col gap-2">
-                    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
-                      {children}
+          <QueryProvider>
+            <WsProvider>
+              <TooltipProvider>
+                <TopProgressBar />
+                <Toaster position="top-right" />
+                <SidebarProvider
+                  style={
+                    {
+                      "--sidebar-width": "calc(var(--spacing) * 72)",
+                      "--header-height": "calc(var(--spacing) * 12)",
+                    } as React.CSSProperties
+                  }
+                >
+                  <AppSidebar variant="inset" />
+                  <SidebarInset>
+                    <SiteHeader />
+                    <div className="flex flex-1 flex-col">
+                      <div className="@container/main flex flex-1 flex-col gap-2">
+                        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
+                          {children}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </SidebarInset>
-            </SidebarProvider>
-            </TooltipProvider>
-          </WsProvider>
-        </QueryProvider>
+                  </SidebarInset>
+                </SidebarProvider>
+              </TooltipProvider>
+            </WsProvider>
+          </QueryProvider>
         </LocaleProvider>
       </body>
     </html>
